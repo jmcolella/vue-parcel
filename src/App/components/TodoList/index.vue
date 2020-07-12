88b64436075762361ca3a7528c0e14aeee364971
John Colella <john.colella@policygenius.com>
2020-07-11 20:31:12 -0400

    Add Todo delete functionality
    - Add handleRemoveTodo function to the TodoList and emit the event to
      the App component
    - Add listener in App component for TodoList removeTodo event
    - Add X "icon" to represent the delete button

--- src/App/components/TodoList/index.vue
+++ src/App/components/TodoList/index.vue
@@ -1,9 +1,12 @@
 <template lang="jade">
   <div>
-    <div v-if="noTodos">Add some Todos!</div>
+    <h1 v-if="noTodos">Add some Todos!</h1>
 
-    <div v-else v-for="todo in todos">
-      <p>{{ todo }}</p>
+    <div v-else v-for="t in todos">
+      <div class="todo-item">
+        <p>{{ t.todo }}</p>
+        <div class="remove-icon" v-on:click="handleRemoveTodo(t.id)"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,11 +14,20 @@
 <script lang="ts">
   import Vue from 'vue'
 
+  interface Methods {
+    handleRemoveTodo: (id: number) => void,
+  }
+
   export default Vue.extend({
     name: 'TodoList',
     props: {
-      todos: Array,
+      todos: Array as () => Todo[],
     },
+    methods: {
+      handleRemoveTodo(id: number) {
+        this.$emit('removeTodo', id);
+      }
+    } as Methods,
     computed: {
       noTodos() {
         return this.$props.todos.length === 0;
@@ -23,3 +35,38 @@
     }
   })
 </script>
+
+<style scoped>
+  .todo-item {
+    align-items: center;
+    display: flex;
+  }
+
+  .remove-icon {
+    height: 10px;
+    margin-left: 5px;
+    position: relative;
+    width: 10px;
+  }
+
+  .remove-icon:hover {
+    cursor: pointer;
+  }
+
+  .remove-icon:before,
+  .remove-icon:after {
+    border: .5px solid black;
+    border-radius: 1px;
+    content: '';
+    height: 10px;
+    position: absolute;
+  }
+
+  .remove-icon:before {
+    transform: translateX(5px) rotate(45deg);
+  }
+
+  .remove-icon:after {
+    transform: translateX(5px) rotate(-45deg);
+  }
+</style>
---
+++ src/types/todo.d.ts
@@ -0,0 +1,4 @@
+declare interface Todo {
+  id: number,
+  todo: string,
+}
