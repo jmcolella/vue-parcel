58e37d7b7989d7ba6e667378f55121a6999a5607
John Colella <john.colella@policygenius.com>
2020-07-11 23:21:48 -0400

    Add edit Todo functionality
    - Add functions in TodoList component to handle editing a Todo item
      inline and submiting that new value to the list of todos
    = Add a TodoItem component to further encapsulate that UI

--- src/App/components/TodoForm/index.vue
+++ src/App/components/TodoForm/index.vue
@@ -1,8 +1,8 @@
 <template lang="jade">
   <form v-on:submit.prevent="onSubmit" name="TodoForm">
-    <TextField v-model="todo" placeholder="Add a Todo"></TextField>
+    <TextField v-model="todo" :placeholder="placeholderText"></TextField>
 
-    <button type="submit">Add Todo</button>
+    <button type="submit">{{ submitButtonText }}</button>
   </form>
 </template>
 
@@ -23,16 +23,36 @@
     name: 'TodoForm',
     data(): Data {
       return {
-        todo: '',
+        todo: this.initialTodo || '',
       };
+    },
+    props: {
+      initialTodo: String,
+      editing: Boolean,
     },
     methods: {
       onSubmit() {
-        this.$emit('addTodo', this.$data.todo);
+        this.$emit('onSubmit', this.$data.todo);
 
         this.$data.todo = '';
       }
     } as Methods,
+    computed: {
+      submitButtonText() {
+        if (this.$props.editing) {
+          return 'Edit Todo';
+        }
+
+        return 'Add Todo';
+      },
+      placeholderText() {
+        if (this.$props.editing) {
+          return 'Edit this Todo';
+        }
+
+        return 'Add a Todo';
+      }
+    },
     components: {
       TextField,
     },
